6737ff03cf5c78e082b9553e181a10bfb16316f5
Tarik Ünal <2108287@talnet.nl>
2020-08-27 15:11:07 +0200

    Deel 1 Taak 1

--- 1-Arrays/taak01/index.php
+++ 1-Arrays/taak01/index.php
@@ -1,1 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$getallen = [45,6546,676,234,8765];
+$namen = ['bart', 'david', 'emiel'];
 
+$medewerker = [
+  'voornaam' => 'Willem',
+  'achternaam' => 'van Oranje',
+  'functie'  => 'koning'
+];
+
+$frisdrank = [
+  'naam' => 'Cola',
+  'merk' => 'Coca Cola',
+  'hoeveelheid'  => '1 liter'
+];
+
+$medewerkers = [
+  [
+    'voornaam' => 'Willem',
+    'achternaam' => 'van Oranje',
+    'functie'  => 'koning'
+  ],
+  [
+    'voornaam' => 'Donald',
+    'achternaam' => 'Trump',
+    'functie'  => 'president'
+  ]
+];
+
+
+echo $medewerkers[0]['voornaam'] . " " . $medewerkers[0]['achternaam'] . " " . "is" . " " . $medewerkers[0]['functie'];
+echo "<br>";
+echo $medewerkers[1]['voornaam']. " " . $medewerkers[1]['achternaam'] . " " . "is" . " " . $medewerkers[1]['functie'];
+
+?> 
+
+</body>
+</html>
---
+++ 1-Arrays/taak02/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$medewerkers = [
+  [
+    'voornaam' => 'Willem',
+    'achternaam' => 'van Oranje',
+    'functie'  => 'koning'
+  ],
+  [
+    'voornaam' => 'Donald',
+    'achternaam' => 'Trump',
+    'functie'  => 'president'
+  ],
+  [
+    'voornaam' => 'Homer',
+    'achternaam' => 'Simpson',
+    'functie'  => 'fabrieksarbeider'
+  ],
+  [
+    'voornaam' => 'Johan',
+    'achternaam' => 'Cruyff',
+    'functie'  => 'voetbalicoon'
+  ]
+  ,
+  [
+    'voornaam' => 'Badr',
+    'achternaam' => 'Hari',
+    'functie'  => 'Kickboxer'
+  ]
+];
+
+/*  De medewerkers-array heeft nu meerdere arrays. */
+
+
+for ($x = 0; $x <= 5; $x++) {
+    echo $medewerkers[0]['voornaam'] . " " . $medewerkers[0]['achternaam'] . " " . "is" . " " . $medewerkers[0]['functie'] . "<br>";
+  }
+?>
+</body>
+</html>
