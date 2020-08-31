d1f9d4482edf0d01557918bfdc977e5308212493
Tarik Ünal <2108287@talnet.nl>
2020-08-31 10:35:26 +0200

    deel 1 taak 4

---
+++ 1-Arrays/taak03/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$medewerkers = [
+  [
+    'voornaam' => 'Willem',
+    'achternaam' => 'van Oranje',
+    'functie'  => 'koning'
+  ],
+  [
+    'voornaam' => 'Donald',
+    'achternaam' => 'Trump',
+    'functie'  => 'president'
+  ],
+  [
+    'voornaam' => 'Homer',
+    'achternaam' => 'Simpson',
+    'functie'  => 'fabrieksarbeider'
+  ],
+  [
+    'voornaam' => 'Johan',
+    'achternaam' => 'Cruyff',
+    'functie'  => 'voetbalicoon'
+  ]
+  ,
+  [
+    'voornaam' => 'Badr',
+    'achternaam' => 'Hari',
+    'functie'  => 'Kickboxer'
+  ]
+];
+foreach($medewerkers as $x) {
+    echo $x['voornaam'] . " ";
+
+    echo $x['achternaam'] . " ";
+    echo $x['functie'] . " ";
+    echo "<br>";
+}
+?>
+</body>
+</html>
