2413d8d411860003c4fd776543ccc58667f8034a
Tarik Ünal <2108287@talnet.nl>
2020-09-03 10:16:18 +0200

    deel 1 taak 4

--- 1-Arrays/taak04/index.php
+++ 1-Arrays/taak04/index.php
@@ -1,6 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
-
-$speelgoed = [
+    $speelgoed = [
 
     [
         'naam' => 'bal',
@@ -37,3 +44,18 @@ $games = [
         'prijs' => '40.00'
     ]
 ];
+
+
+echo "Speelgoed" . " " . $speelgoed[0]['naam'] . " " . "uit de categorie" . " " . $speelgoed[0]['soort'] . " " . "is" . " " . $speelgoed[0]['prijs'] . "<br>";
+echo "Speelgoed" . " " . $speelgoed[1]['naam'] . " " . "uit de categorie" . " " . $speelgoed[1]['soort'] . " " . "is" . " " . $speelgoed[1]['prijs'] . "<br>";
+echo "Speelgoed" . " " . $speelgoed[2]['naam'] . " " . "uit de categorie" . " " . $speelgoed[2]['soort'] . " " . "is" . " " . $speelgoed[2]['prijs'] . "<br>";
+echo "Speelgoed" . " " . $games['sport']['naam'] . " " . "van uitgever" . " " . $games['sport']['uitgever'] . " " . "is" . " " . $games['sport']['prijs'] . "<br>";
+echo "Speelgoed" . " " . $games['action']['naam'] . " " . "van uitgever" . " " . $games['action']['uitgever'] . " " . "is" . " " . $games['action']['prijs'] . "<br>";
+echo "Speelgoed" . " " . $games['fps']['naam'] . " " . "van uitgever" . " " . $games['fps']['uitgever'] . " " . "is" . " " . $games['fps']['prijs'] . "<br>";
+
+
+?>
+</body>
+</html>
+
+
