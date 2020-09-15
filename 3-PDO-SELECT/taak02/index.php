de81a119ab1f0be01c955329cb634f489f75520d
Tarik Ünal <2108287@talnet.nl>
2020-09-15 10:51:04 +0200

    deel 3 taak 1

---
+++ 3-PDO-SELECT/taak02/index.php
@@ -0,0 +1,17 @@
+<?php
+$db_loc = 'localhost';
+$db_name = 'toolsforever';
+$db_user = 'root';
+$db_pw = '';
+
+$db_conn = new PDO("mysql:host=$db_loc;dbname=$db_name", $db_user, $db_pw);
+
+$sql = "SELECT * FROM users";
+$statement = $db_conn->prepare($sql); //haal alle gebruikers op uit de database toolsforever
+$statement->execute();
+$database_gegevens = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach($database_gegevens as $users){  
+  echo $users['firstname'] . "<br>";
+}
+?>
